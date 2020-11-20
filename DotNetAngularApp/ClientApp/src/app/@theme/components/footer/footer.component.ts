97cd50aaa138ef42088cf941845c79430e72c003
Harith Sufri <as.siddiqeen@gmail.com>
2020-11-20 13:10:00 +0800

    Add 'Back' word in edit Booking

--- DotNetAngularApp/ClientApp/src/app/@theme/components/footer/footer.component.ts
+++ DotNetAngularApp/ClientApp/src/app/@theme/components/footer/footer.component.ts
@@ -5,11 +5,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./footer.component.scss'],
   template: `
   <span class="created-by">
-    <b>Universiti Brunei Darussalam</b> &copy; 2020
+  <a href="https://ubd.edu.bn"><b>Universiti Brunei Darussalam</b></a> &copy; 2020
   </span>
-    <div class="socials">
-      <a href="https://github.com/kuroza/UBD-TABS" target="_blank" class="ion ion-social-github"></a>
-    </div>
+  <!-- <div class="socials">
+    <a href="https://github.com/kuroza/UBD-TABS" target="_blank" class="ion ion-social-github"></a>
+  </div> -->
   `,
 })
 export class FooterComponent {
